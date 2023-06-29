43fb121de9771bf1d80c65debec472a4f21ccff3
dtthien2807 <dtthien2807@gmail.com>
2023-06-29 22:43:07 +0700

    category product

---
+++ app/src/main/java/com/example/flowerapp/CategoryAdminActivity.java
@@ -0,0 +1,14 @@
+package com.example.flowerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_admin);
+    }
+}
