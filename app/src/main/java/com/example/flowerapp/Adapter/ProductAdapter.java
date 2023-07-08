c4c74f0fd30403fb6d16e20f0152bb628da4f20c
dtthien2807 <dtthien2807@gmail.com>
2023-07-08 15:52:12 +0700

    add new

---
+++ app/src/main/java/com/example/flowerapp/Adapter/ProductAdapter.java
@@ -0,0 +1,74 @@
+package com.example.flowerapp.Adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.flowerapp.Entity.Flower;
+import com.example.flowerapp.R;
+
+import java.util.List;
+
+public class ProductAdapter extends ArrayAdapter {
+    private Activity context;
+    private List<Flower> flowerList;
+    private IClickListener mIClickListener;
+    public interface IClickListener {
+        void onClickEditProduct(Flower flower);
+        void onClickDeleteProduct(Flower flower);
+    }
+    public ProductAdapter(@NonNull Activity context, List<Flower> flowerList, IClickListener iClickListener) {
+        super(context, R.layout.activity_listview_product, flowerList);
+        this.context = context;
+        this.flowerList = flowerList;
+        this.mIClickListener = iClickListener;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View ListItem = inflater.inflate(R.layout.activity_listview_product,null,true);
+        TextView txtId = ListItem.findViewById(R.id.txtID);
+        TextView txtName = ListItem.findViewById(R.id.txtNameProduct);
+        ImageView imgFlower = ListItem.findViewById(R.id.imgProduct);
+        TextView txtDescription = ListItem.findViewById(R.id.txtDescription);
+        TextView txtQuality = ListItem.findViewById(R.id.txtQuality);
+        TextView txtPrice = ListItem.findViewById(R.id.txtPrice);
+        TextView txtDate = ListItem.findViewById(R.id.txtDate);
+        Button btn_edit = ListItem.findViewById(R.id.btnEdit);
+        Button btn_del = ListItem.findViewById(R.id.btnDel);
+
+        Flower flower = flowerList.get(position);
+
+        txtId.setText(flower.getId_flower());
+        txtName.setText(flower.getName());
+        txtDescription.setText("Mô tả: "+flower.getDescription());
+        txtQuality.setText("Số lượng: "+String.valueOf(flower.getQuantity()));
+        txtPrice.setText("Gía tiền: "+String.valueOf(flower.getPrice()));
+        txtDate.setText("Date: "+flower.getCreated_at());
+
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIClickListener.onClickEditProduct(flower);
+            }
+        });
+        btn_del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIClickListener.onClickDeleteProduct(flower);
+            }
+        });
+
+        return ListItem;
+    }
+}
--- app/src/main/java/com/example/flowerapp/ProductAdminActivity.java
+++ app/src/main/java/com/example/flowerapp/ProductAdminActivity.java
@@ -1,14 +1,355 @@
 package com.example.flowerapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Base64;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.flowerapp.Adapter.ProductAdapter;
+import com.example.flowerapp.Entity.Flower;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ProductAdminActivity extends AppCompatActivity {
 
+    DatabaseReference databasecategory;
+    GridView productListView;
+    List<Flower> flowerList;
+    ImageView imgUpload;
+    public static final int SELECT_PICTURE = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_admin);
+        productListView = findViewById(R.id.list_product);
+        flowerList = new ArrayList<>();
+        databasecategory = FirebaseDatabase.getInstance().getReference("Cate_Flower");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = getIntent();
+        String id_category = intent.getStringExtra("id_category");
+        databasecategory.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                flowerList.clear();
+                if(snapshot.exists()) {
+                    for (DataSnapshot categorysnapshot : snapshot.getChildren()) {
+                        if (categorysnapshot.getKey().equals(id_category)) {
+                            DataSnapshot flowerSnapshot = categorysnapshot.child("Flower");
+                            for (DataSnapshot flowerDocument : flowerSnapshot.getChildren()) {
+                                Boolean checkStatus = flowerDocument.child("status").getValue(Boolean.class);
+                                if(checkStatus) {
+                                    Flower flower = flowerDocument.getValue(Flower.class);
+                                    flowerList.add(flower);
+                                }
+                            }
+                        }
+                    }
+                    ProductAdapter adaptor = new ProductAdapter(ProductAdminActivity.this, flowerList, new ProductAdapter.IClickListener() {
+                        @Override
+                        public void onClickEditProduct(Flower flower) {
+                            openDialogEdit(flower);
+                        }
+
+                        @Override
+                        public void onClickDeleteProduct(Flower flower) {
+                            openDialogDelete(flower);
+                        }
+                    });
+                    adaptor.notifyDataSetChanged();
+                    productListView.setAdapter(adaptor);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void openDialogDelete(Flower flower) {
+        Intent intent = getIntent();
+        String id_category = intent.getStringExtra("id_category");
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_dialog_delete_flower);
+        Window window = dialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+
+        Button btn_del = dialog.findViewById(R.id.btn_del);
+        Button btn_cancel = dialog.findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btn_del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databasecategory = FirebaseDatabase.getInstance().getReference("Cate_Flower").child(id_category).child("Flower");
+                flower.setStatus(false);
+                databasecategory.child(String.valueOf(flower.getId_flower())).updateChildren(flower.toMap(), new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                        Toast.makeText(ProductAdminActivity.this, "You have deleted this product!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void openDialogEdit(Flower flower) {
+        Intent intent = getIntent();
+        String id_category = intent.getStringExtra("id_category");
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_dialog_add_flower);
+        Window window = dialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+
+        Button btn_add = dialog.findViewById(R.id.btn_add);
+        Button btn_cancel = dialog.findViewById(R.id.btn_cancel);
+        EditText txtName = dialog.findViewById(R.id.txtName);
+        EditText txtDescription = dialog.findViewById(R.id.txtDescription);
+        EditText txtQuantity = dialog.findViewById(R.id.txtQuantity);
+        EditText txtPrice = dialog.findViewById(R.id.txtPrice);
+        Button btnUpload = dialog.findViewById(R.id.btnUpload);
+        imgUpload = dialog.findViewById(R.id.imgUpload);
+
+        txtName.setText(flower.getName());
+        txtDescription.setText(flower.getDescription());
+        txtQuantity.setText(String.valueOf(flower.getQuantity()));
+        txtPrice.setText(String.valueOf(flower.getPrice()));
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+
+        });
+
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databasecategory = FirebaseDatabase.getInstance().getReference("Cate_Flower").child(id_category).child("Flower");
+                String newName = txtName.getText().toString().trim();
+                String description = txtDescription.getText().toString().trim();
+                Float quantity = Float.valueOf(txtQuantity.getText().toString().trim());
+                Float price = Float.valueOf(txtPrice.getText().toString().trim());
+
+                flower.setName(newName);
+                flower.setDescription(description);
+                flower.setQuantity(quantity);
+                flower.setPrice(price);
+                databasecategory.child(String.valueOf(flower.getId_flower())).updateChildren(flower.toMap(), new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                        Toast.makeText(ProductAdminActivity.this, "You have updated successful!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+        dialog.show();
+
+    }
+
+    public void openDialogAddNewFlower(View view) {
+        Intent intent = getIntent();
+        String id_category = intent.getStringExtra("id_category");
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_dialog_add_flower);
+        Window window = dialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+
+        Button btn_add = dialog.findViewById(R.id.btn_add);
+        Button btn_cancel = dialog.findViewById(R.id.btn_cancel);
+        EditText txtName = dialog.findViewById(R.id.txtName);
+        EditText txtDescription = dialog.findViewById(R.id.txtDescription);
+        EditText txtQuantity = dialog.findViewById(R.id.txtQuantity);
+        EditText txtPrice = dialog.findViewById(R.id.txtPrice);
+        Button btnUpload = dialog.findViewById(R.id.btnUpload);
+        imgUpload = dialog.findViewById(R.id.imgUpload);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+
+        });
+
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databasecategory = FirebaseDatabase.getInstance().getReference("Cate_Flower").child(id_category).child("Flower");
+                String key = databasecategory.push().getKey();
+                databasecategory.child(key).removeValue();
+                String newName = txtName.getText().toString().trim();
+                String description = txtDescription.getText().toString().trim();
+                Float quantity = Float.valueOf(txtQuantity.getText().toString().trim());
+                Float price = Float.valueOf(txtPrice.getText().toString().trim());
+//                Bitmap bitmap = ((BitmapDrawable)imgUpload.getDrawable()).getBitmap();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = new Date();
+                String created_at = dateFormat.format(date);
+                databasecategory.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Flower flower = new Flower();
+                        flower.setId_flower(key);
+                        flower.setName(newName);
+                        flower.setDescription(description);
+                        flower.setQuantity(quantity);
+                        flower.setPrice(price);
+                        flower.setCreated_at(created_at);
+                        flower.setStatus(true);
+//                        flower.setUrl(bitmap);
+                        databasecategory.child(key).setValue(flower);
+                        Toast.makeText(ProductAdminActivity.this, "You have add successfully!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+        dialog.show();
+    }
+    private void imageChooser()
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        launchSomeActivity.launch(i);
+    }
+    ActivityResultLauncher<Intent> launchSomeActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap = null;
+                        try {
+                            selectedImageBitmap = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        imgUpload.setImageBitmap(selectedImageBitmap);
+                        String picturePath = getFileName(selectedImageUri );
+                        Toast.makeText(getApplicationContext(), "updated "+picturePath,Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+
+    public void revert(View view) {
+        Intent intent = new Intent(this, CategoryAdminActivity.class);
+        startActivity(intent);
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+    public String getStringImage(Bitmap bm){
+        ByteArrayOutputStream ba=new ByteArrayOutputStream(  );
+        bm.compress( Bitmap.CompressFormat.PNG,90,ba );
+        byte[] by=ba.toByteArray();
+        String encod= Base64.encodeToString( by,Base64.DEFAULT );
+        return encod;
     }
 }
